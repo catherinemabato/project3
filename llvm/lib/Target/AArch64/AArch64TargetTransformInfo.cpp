a1c5378a5a017b9eec26c49eed98a825ddc93351
Hassnaa Hamdi <hassnaa.hamdi@arm.com>
2024-10-08 00:08:35 +0000

    [resolve review comments] do multiple ordering during (de)interleaving to put the nodes in the correct order for (de)interleaving.
    Change-Id: I151a53c459a7f69e35feb428c1dface2fe57e9ce

--- llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
@@ -3558,9 +3558,7 @@ InstructionCost AArch64TTIImpl::getInterleavedMemoryOpCost(
   assert(Factor >= 2 && "Invalid interleave factor");
   auto *VecVTy = cast<VectorType>(VecTy);
 
-  unsigned MaxFactor = TLI->getMaxSupportedInterleaveFactor();
-  if (VecTy->isScalableTy() &&
-      (!ST->hasSVE() || !isPowerOf2_32(Factor) || Factor > MaxFactor))
+  if (VecTy->isScalableTy() && (!ST->hasSVE() || (Factor != 2 && Factor != 4)))
     return InstructionCost::getInvalid();
 
   // Vectorization for masked interleaved accesses is only enabled for scalable
@@ -3568,7 +3566,7 @@ InstructionCost AArch64TTIImpl::getInterleavedMemoryOpCost(
   if (!VecTy->isScalableTy() && (UseMaskForCond || UseMaskForGaps))
     return InstructionCost::getInvalid();
 
-  if (!UseMaskForGaps && Factor <= MaxFactor) {
+  if (!UseMaskForGaps && Factor <= TLI->getMaxSupportedInterleaveFactor()) {
     unsigned MinElts = VecVTy->getElementCount().getKnownMinValue();
     auto *SubVecTy =
         VectorType::get(VecVTy->getElementType(),
